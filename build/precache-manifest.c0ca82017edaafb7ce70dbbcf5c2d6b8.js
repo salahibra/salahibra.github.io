7f6362d40bdfd6cca591b2fbf4d2bc772cd9d445
ibrahim salah <ibrahimsalahzefri75@gmail.com>
2024-12-08 03:00:07 +0100

    second commit

---
+++ build/precache-manifest.c0ca82017edaafb7ce70dbbcf5c2d6b8.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    revision: "6b774af7635369c6c716fccf9bc4886b",
+    url: "/index.html",
+  },
+  {
+    revision: "286428741d6bf4ebe157",
+    url: "/static/css/main.113fbee8.chunk.css",
+  },
+  {
+    revision: "cdac256da4adf0698e56",
+    url: "/static/js/2.4effa3fe.chunk.js",
+  },
+  {
+    revision: "286428741d6bf4ebe157",
+    url: "/static/js/main.6173daab.chunk.js",
+  },
+  {
+    revision: "401a55cfa891f62095fd",
+    url: "/static/js/runtime-main.37723130.js",
+  },
+  {
+    revision: "21f233e19402cc4a66866a7f31191f0d",
+    url: "/static/media/Agustina.21f233e1.woff",
+  },
+  {
+    revision: "5b521b0501ec2548fdc7b30663c33c80",
+    url: "/static/media/Alison.5b521b05.svg",
+  },
+  {
+    revision: "4457817ac2b9993c65e81aa05828fe9c",
+    url: "/static/media/GoogleSans-Bold.4457817a.ttf",
+  },
+  {
+    revision: "90773b6158663ab0fe78b32680733677",
+    url: "/static/media/GoogleSans-BoldItalic.90773b61.ttf",
+  },
+  {
+    revision: "0ecddcdeccb7761ce899aa9ad9f0ac3f",
+    url: "/static/media/GoogleSans-Italic.0ecddcde.ttf",
+  },
+  {
+    revision: "8d57e4014b18edef070d285746485115",
+    url: "/static/media/GoogleSans-Medium.8d57e401.ttf",
+  },
+  {
+    revision: "8fd3737925e83c87d78a13700ccf9a62",
+    url: "/static/media/GoogleSans-MediumItalic.8fd37379.ttf",
+  },
+  {
+    revision: "b5c77a6aed75cdad9489effd0d5ea411",
+    url: "/static/media/GoogleSans-Regular.b5c77a6a.ttf",
+  },
+  {
+    revision: "ee6539921d713482b8ccd4d0d23961bb",
+    url: "/static/media/Montserrat-Regular.ee653992.ttf",
+  },
+  {
+    revision: "9c12841bcd3f0e2b2cfeaeb67f8d43a3",
+    url: "/static/media/Python-for-CPGE.9c12841b.png",
+  },
+  {
+    revision: "16559172ca1d4d552390b48f19e102ba",
+    url: "/static/media/address_image.16559172.svg",
+  },
+  {
+    revision: "058eca4cbdc03a1dad9a6bb943ac7831",
+    url: "/static/media/blogs_image.058eca4c.svg",
+  },
+  {
+    revision: "7169eafb412bf2df21fd61a482a2c136",
+    url: "/static/media/coursera.7169eafb.png",
+  },
+  {
+    revision: "f3a04d553dd88a3b6dfd4dff2b992c7d",
+    url: "/static/media/cpge.f3a04d55.svg",
+  },
+  {
+    revision: "3bab7a9dc4324f1b2364d25a47fcfcf0",
+    url: "/static/media/education.3bab7a9d.svg",
+  },
+  {
+    revision: "a874f38eb1d42873c195d6cd3fef9ebd",
+    url: "/static/media/ensam.a874f38e.svg",
+  },
+  {
+    revision: "a00ede3f70759480e81f14f203edaa51",
+    url: "/static/media/experience.a00ede3f.svg",
+  },
+  {
+    revision: "0017cc6de3f72f67753efa37bb796fbd",
+    url: "/static/media/fa-brands-400.0017cc6d.svg",
+  },
+  {
+    revision: "06147b6cd88c7346cecd1edd060cd5de",
+    url: "/static/media/fa-brands-400.06147b6c.ttf",
+  },
+  {
+    revision: "5063b105c7646c8043d58c5289f02cca",
+    url: "/static/media/fa-brands-400.5063b105.eot",
+  },
+  {
+    revision: "c5e0f14f88a828261ba01558ce2bf26f",
+    url: "/static/media/fa-brands-400.c5e0f14f.woff",
+  },
+  {
+    revision: "cccc9d29470e879e40eb70249d9a2705",
+    url: "/static/media/fa-brands-400.cccc9d29.woff2",
+  },
+  {
+    revision: "65b286af947c0d982ca01b40e1fcab38",
+    url: "/static/media/fa-regular-400.65b286af.ttf",
+  },
+  {
+    revision: "c1a866ec0e04a5e1915b41fcf261457c",
+    url: "/static/media/fa-regular-400.c1a866ec.eot",
+  },
+  {
+    revision: "c4f508e7c4f01a9eeba7f08155cde04e",
+    url: "/static/media/fa-regular-400.c4f508e7.woff",
+  },
+  {
+    revision: "e647ddbb6d95cb01deac07ffafb2a085",
+    url: "/static/media/fa-regular-400.e647ddbb.svg",
+  },
+  {
+    revision: "f5f2566b93e89391da4db79462b8078b",
+    url: "/static/media/fa-regular-400.f5f2566b.woff2",
+  },
+  {
+    revision: "0bff33a5fd7ec390235476b4859747a0",
+    url: "/static/media/fa-solid-900.0bff33a5.ttf",
+  },
+  {
+    revision: "333bae208dc363746961b234ff6c2500",
+    url: "/static/media/fa-solid-900.333bae20.woff",
+  },
+  {
+    revision: "424c59fb1df79fcc3d0a41909c0192d2",
+    url: "/static/media/fa-solid-900.424c59fb.svg",
+  },
+  {
+    revision: "44d537ab79f921fde5a28b2c1636f397",
+    url: "/static/media/fa-solid-900.44d537ab.woff2",
+  },
+  {
+    revision: "8e4a6dcc692b3887f9f542cd6894d6d4",
+    url: "/static/media/fa-solid-900.8e4a6dcc.eot",
+  },
+  {
+    revision: "2936ce95f550e3feef7230ea543a4b60",
+    url: "/static/media/feelingProud.2936ce95.svg",
+  },
+  {
+    revision: "fce6080726685cabdb4f75a0d8018325",
+    url: "/static/media/fullstack.fce60807.svg",
+  },
+  {
+    revision: "7c7fa2a9a2661df4559a0ed7ff930e44",
+    url: "/static/media/hegroupe.7c7fa2a9.png",
+  },
+  {
+    revision: "b73ab374dbd19d55a8b0a34606c5c1ec",
+    url: "/static/media/hp-life.b73ab374.svg",
+  },
+  {
+    revision: "af3f3620cc63814a972279ec3efbf13a",
+    url: "/static/media/lahcen.af3f3620.png",
+  },
+  {
+    revision: "028ec89a63527917879d40ac157f8857",
+    url: "/static/media/mne.028ec89a.svg",
+  },
+  {
+    revision: "b8ba948796d7ab532673c5ed2f315e74",
+    url: "/static/media/projects_image.b8ba9487.svg",
+  },
+]);
